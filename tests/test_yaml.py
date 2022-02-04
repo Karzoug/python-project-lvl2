e6d0e75e7b0cecd88c3dd0782731fc1caf58e970
hellhamster <a.n.baranov@outlook.com>
2022-02-04 14:19:43 +0300

    change python vers

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -8,5 +8,4 @@ def test_yaml_fun():
     with open(
         'tests/fixtures/right_result_yaml.txt', 'r') as right_result1_file:
         right_result1 = right_result1_file.read()
-        # print('right from file:', right_result1[:-1])
         assert generate_diff(test1, test2) == right_result1[:-1]
